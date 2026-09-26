ceefad98afbd140e9bb6745d69aa056c60a39a0e
Aman Singh Bisht <amansinghbisht.dev@gmail.com>
2026-09-26 13:12:12 +0530

    feat: add resizable functionality with ResizableWrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import DraggableWrapper from "./components/DraggableWrapper";
+import ResizableWrapper from "./components/ResizableWrapper";
 
 const App = () => {
   return (
-    <div className="h-screen w-screen bg-black">
-      <DraggableWrapper>
-        <div className="bg-amber-50 w-40 text-center p-5"> Drag meee!! </div>
+    <div className="h-screen w-screen bg-black overflow-hidden relative">
+      <DraggableWrapper defaultPosition={{ x: 50, y: 50 }}>
+        <ResizableWrapper className="flex items-center justify-center">
+          <div className="p-5 w-full h-full bg-purple-800 text-white font-extrabold flex items-center justify-center">
+            Drag and Resize me!!
+          </div>
+        </ResizableWrapper>
+      </DraggableWrapper>
+
+      <ResizableWrapper
+        className="flex items-center justify-center"
+        style={{ top: "250px", left: "50px" }}
+      >
+        <div className="p-5 w-full h-full bg-red-800 text-white font-extrabold flex items-center justify-center">
+          Resize me!!
+        </div>
+      </ResizableWrapper>
+
+      <DraggableWrapper defaultPosition={{ x: 300, y: 50 }}>
+        <div>searchbar</div>
       </DraggableWrapper>
     </div>
   );
--- src/components/DraggableWrapper.jsx
+++ src/components/DraggableWrapper.jsx
@@ -1,11 +1,16 @@
 import React, { useRef } from "react";
 import Draggable from "react-draggable";
 
-const DraggableWrapper = ({ children }) => {
+const DraggableWrapper = ({ children, defaultPosition}) => {
   const nodeRef = useRef(null);
 
   return (
-    <Draggable nodeRef={nodeRef} bounds="parent">
+    <Draggable
+      nodeRef={nodeRef}
+      bounds="parent"
+      cancel=".react-resizable-handle"
+      defaultPosition={defaultPosition}
+    >
       {React.cloneElement(children, { ref: nodeRef })}
     </Draggable>
   );
